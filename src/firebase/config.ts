d7e4b16b873d8a1fc6302293988673d3c4adff77
Mauro Patocchi <mauro.tt.patocchi@gmail.com>
2026-03-03 22:14:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9184228626-36cd9",
+  "appId": "1:75399338334:web:3bad9812f514390689d7cf",
+  "apiKey": "AIzaSyCO_AbsMho2PD6o2FMCkVd-QGNlCQXph2w",
+  "authDomain": "studio-9184228626-36cd9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "75399338334"
+};
